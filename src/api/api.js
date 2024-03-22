0270b55041dad4674abfc7fe059ae5b55ea1c2bb
chebishev <atanas.chebishev@gmail.com>
2024-03-22 21:39:44 +0200

    fix url

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,6 @@
-import { config } from "process";
 import * as config from "../config.js";
 
-export const settings = config.settings.host;
+export const settings = config.settings;
 
 async function request(url, options) {
   try {
